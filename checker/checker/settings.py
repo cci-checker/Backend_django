c6f6965cf433e235e787af43cccdb4850daf390f
Tushar Chawla <chawla.tushar5@gmail.com>
2019-02-10 23:02:59 -0500

    Heroku changes

--- checker/checker/settings.py
+++ checker/checker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+django_heroku.settings(locals())
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
